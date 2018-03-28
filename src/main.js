bbbb3e0c2d55a8085e73a13e9860b2f0bdf6fe34
Lucas Flóro <lucas@fearp.usp.br>
2018-03-28 11:00:22 -0300

    Test Husk prepush

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const a = 3;
-const b = 2;
+const b = 2
 const c = 'adfasfasdkfjaslkfjaf;kasjfa;lksfafjasdlkfjasklfjasklfjaslkfjas;dfapoieshjlasdkhfa;lsdfja;lkfjasd;lkfjasd;lkfja;lfkjd;lkfajs;dlkfdjas;lkfjas;lfkjas;lkfjas;lkfjas;flkajsf;lkasjf;lkasjfa;lksdfjaslk;fjasd;lkfjasdlkfj;asdkfjas';
 
 function sum() {
