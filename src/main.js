856d1c7a0c41bf5eaa7bb177711c19a0c766e544
Lucas Flóro <lucas@fearp.usp.br>
2018-03-28 10:53:58 -0300

    Add main js file

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const a = 3;
+const b = 2;
+const c = 'adfasfasdkfjaslkfjaf;kasjfa;lksfafjasdlkfjasklfjasklfjaslkfjas;dfapoieshjlasdkhfa;lsdfja;lkfjasd;lkfjasd;lkfja;lfkjd;lkfajs;dlkfdjas;lkfjas;lfkjas;lkfjas;lkfjas;flkajsf;lkasjf;lkasjfa;lksdfjaslk;fjasd;lkfjasdlkfj;asdkfjas';
+
+function sum() {
+  return a + b + c;
+}
+
+sum()
